9d50c8aae4f74417dbf9217295cc2e50254d372d
Santiago <santimh04@gmail.com>
2024-12-08 14:52:06 -0500

    Feat: speciality branches added

--- src/main/java/com/riwi/io/employabilityassessment_santiagomarinhiguita/utils/SpecialityBranch.java
+++ src/main/java/com/riwi/io/employabilityassessment_santiagomarinhiguita/utils/SpecialityBranch.java
@@ -1,4 +1,9 @@
 package com.riwi.io.employabilityassessment_santiagomarinhiguita.utils;
 
 public enum SpecialityBranch {
+    GYNECOLOGY,
+    NEUROLOGY,
+    NUTRITION,
+    ONCOLOGY,
+    PEDIATRIC,
 }
