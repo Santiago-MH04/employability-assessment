b555e70fc23675b2c3112d9b7b365db706b01c41
Santiago <santimh04@gmail.com>
2024-12-07 20:52:17 -0500

    feat: project initialisation

---
+++ src/main/java/com/riwi/io/employabilityassessment_santiagomarinhiguita/EmployabilityAssessmentSantiagoMarinHiguitaApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.io.employabilityassessment_santiagomarinhiguita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployabilityAssessmentSantiagoMarinHiguitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployabilityAssessmentSantiagoMarinHiguitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/io/employabilityassessment_santiagomarinhiguita/EmployabilityAssessmentSantiagoMarinHiguitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.io.employabilityassessment_santiagomarinhiguita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployabilityAssessmentSantiagoMarinHiguitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
